19006318d5b152542b4121b21da08f9d4aacfe78
nywleswoey <nywleswoey@users.noreply.github.com>
2023-03-09 01:33:17 +0000

    Automated update of themes on token change

--- src/themes/brandBTheme.ts
+++ src/themes/brandBTheme.ts
@@ -2,5 +2,5 @@ import { Theme, webLightTheme } from '@fluentui/react-components';
 
 export const brandBTheme: Theme = {
   ...webLightTheme,
-  colorBrandForeground1: '#115ea4'
+  colorBrandForeground1: '#115ea3'
 };
